338fe2d7a39b83c1c68d543238873db357425a36
bmwasonga <bmwasonga@gmail.com>
2021-04-23 21:47:33 +0300

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { Container, Content, BgImage } from './styles/styles';
+import {
+  Container,
+  Content,
+  BgImage,
+  CTA,
+  CTAlogoOne,
+  Signup,
+  Description,
+  CTAlogotwo,
+} from './styles/styles';
 
 function Login(props) {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTAlogoOne src="/images/cta-logo-one.svg" alt="logo one" />
+          <Signup> GET ALL THERE</Signup>
+          <Description>
+            Get Premier access to Raya and the last Dragon for an additional fee
+            with a Disney+ subscription. As of 3/26/2021, the price of Disney+
+            and The Disney Bundle will increase by 1$.
+          </Description>
+          <CTAlogotwo src="/images/cta-logo-two.png" alt="logo two" />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
--- src/components/Login/styles/styles.js
+++ src/components/Login/styles/styles.js
@@ -30,5 +30,66 @@ export const BgImage = styled.div`
   position: absolute;
   top: 0;
   right: 0;
+  left: 0;
   z-index: -1;
+`;
+
+export const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`;
+
+export const CTAlogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+export const Signup = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.25px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+export const Description = styled.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  /* increase the font size */
+  letter-spacing: 1.5px;
+`;
+
+export const CTAlogotwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%auto;
 `;
