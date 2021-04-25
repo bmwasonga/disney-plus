024b60fc58e5f0f65e54cb019b30d10d0cb5b1f3
bmwasonga <bmwasonga@gmail.com>
2021-04-25 15:34:57 +0300

    nav items added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,6 +13,26 @@ function Header(props) {
             <img src="/images/home-icon.svg" alt="HOME" />
             <span>HOME</span>
           </a>
+          <a href="/home">
+            <img src="/images/search-icon.svg" alt="HOME" />
+            <span>SEARCH</span>
+          </a>
+          <a href="/home">
+            <img src="/images/watchlist-icon.svg" alt="HOME" />
+            <span>WATCHLIST</span>
+          </a>
+          <a href="/home">
+            <img src="/images/original-icon.svg" alt="HOME" />
+            <span>ORIGINALS</span>
+          </a>
+          <a href="/home">
+            <img src="/images/movie-icon.svg" alt="HOME" />
+            <span>MOVIES</span>
+          </a>
+          <a href="/home">
+            <img src="/images/series-icon.svg" alt="HOME" />
+            <span>SERIES</span>
+          </a>
         </NavMenu>
         <Button>Login</Button>
       </Container>
