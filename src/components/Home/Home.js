cd9faecfe924d5479b8f3d7f54391751a8561dea
bmwasonga <bmwasonga@gmail.com>
2021-04-26 23:33:45 +0300

    another commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImgSlider from '../ImgSlider/ImgSlider';
+import Viewers from '../Viewers/Viewers';
 import { Container } from './styles/styles';
 
-function Home() {
+function Home(props) {
   return (
     <Container>
       <ImgSlider />
+      <Viewers />
     </Container>
   );
 }
---
+++ src/components/Viewers/Viewers.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Wrap } from './styles/styles';
+
+function Viewers(props) {
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoplay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoplay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoplay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676296-national-geographic.mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoplay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoplay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" />
+        </video>
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Viewers;
